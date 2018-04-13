73cf45235ab37bcaf02e671f3aec82e793167cc7
WilliamsD2018 <dwilliams@sjnma.org>
2018-04-13 06:16:02 -0500

    First part of code

---
+++ Perfectbodyweight.py
@@ -0,0 +1,10 @@
+##The meaning of this program is for a person to have a better understanding of himself healthwise.
+##It will tell him factors about its body and things hr should work on.
+
+
+height = float(input("Input your height in meters: "))
+weight = float(input("Input your weight in kilogram: "))
+print("Your body mass index is: ", round(weight / (height * height),2))
+
+
+
